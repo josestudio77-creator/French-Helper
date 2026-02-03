c1fb8b4e95a34ff38616835c4487ed602256cfb2
josestudio77-creator <josestudio77@gmail.com>
2026-02-03 16:11:41 -0500

    Upgrade service worker to version 19

--- sw.js
+++ sw.js
@@ -1,20 +1,6 @@
-// sw.js - Service Worker V18
-const CACHE_NAME = 'french-helper-v18';
+// sw.js - Service Worker V19
+const CACHE_NAME = 'french-helper-v19';
 const urlsToCache = ['./', './index.html', './icon.png', './manifest.json'];
-
-self.addEventListener('install', e => {
-  self.skipWaiting(); 
-  e.waitUntil(caches.open(CACHE_NAME).then(c => c.addAll(urlsToCache)));
-});
-
-self.addEventListener('activate', e => {
-  e.waitUntil(
-    caches.keys().then(keys => Promise.all(
-      keys.map(k => { if(k !== CACHE_NAME) return caches.delete(k); })
-    )).then(() => self.clients.claim())
-  );
-});
-
-self.addEventListener('fetch', e => {
-  e.respondWith(fetch(e.request).catch(() => caches.match(e.request)));
-});
+self.addEventListener('install', e => { self.skipWaiting(); e.waitUntil(caches.open(CACHE_NAME).then(c => c.addAll(urlsToCache))); });
+self.addEventListener('activate', e => e.waitUntil(caches.keys().then(keys => Promise.all(keys.map(k => { if(k !== CACHE_NAME) return caches.delete(k); })))));
+self.addEventListener('fetch', e => { e.respondWith(fetch(e.request).catch(() => caches.match(e.request))); });
