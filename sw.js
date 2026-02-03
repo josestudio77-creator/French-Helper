bf3dd623910ab549c0bca08860c70fb3e73eb73b
josestudio77-creator <josestudio77@gmail.com>
2026-02-03 16:45:12 -0500

    Update service worker to version 22

--- sw.js
+++ sw.js
@@ -1,5 +1,5 @@
-// sw.js - Service Worker V21
-const CACHE_NAME = 'french-helper-v21';
+// sw.js - Service Worker V22
+const CACHE_NAME = 'french-helper-v22';
 const urlsToCache = ['./', './index.html', './icon.png', './manifest.json'];
 self.addEventListener('install', e => { self.skipWaiting(); e.waitUntil(caches.open(CACHE_NAME).then(c => c.addAll(urlsToCache))); });
 self.addEventListener('activate', e => e.waitUntil(caches.keys().then(keys => Promise.all(keys.map(k => { if(k !== CACHE_NAME) return caches.delete(k); }))).then(() => self.clients.claim())));
