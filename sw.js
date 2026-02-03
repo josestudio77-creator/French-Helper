13a663fdc642a7ae1d196ee0d077d089988231e1
josestudio77-creator <josestudio77@gmail.com>
2026-02-03 15:37:24 -0500

    Upgrade Service Worker to version 14

--- sw.js
+++ sw.js
@@ -1,5 +1,5 @@
-// sw.js - Service Worker V13
-const CACHE_NAME = 'french-helper-v13';
+// sw.js - Service Worker V14
+const CACHE_NAME = 'french-helper-v14';
 const urlsToCache = ['./', './index.html', './icon.png', './manifest.json'];
 self.addEventListener('install', e => { self.skipWaiting(); e.waitUntil(caches.open(CACHE_NAME).then(c => c.addAll(urlsToCache))); });
 self.addEventListener('activate', e => e.waitUntil(caches.keys().then(keys => Promise.all(keys.map(k => { if(k !== CACHE_NAME) return caches.delete(k); })))));
