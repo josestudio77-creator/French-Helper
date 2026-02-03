dc6d85252a45c38905deed771ae0f908695e599f
josestudio77-creator <josestudio77@gmail.com>
2026-02-03 16:23:52 -0500

    Upgrade service worker to version 20

--- sw.js
+++ sw.js
@@ -1,6 +1,6 @@
-// sw.js - Service Worker V19
-const CACHE_NAME = 'french-helper-v19';
+// sw.js - Service Worker V20
+const CACHE_NAME = 'french-helper-v20';
 const urlsToCache = ['./', './index.html', './icon.png', './manifest.json'];
 self.addEventListener('install', e => { self.skipWaiting(); e.waitUntil(caches.open(CACHE_NAME).then(c => c.addAll(urlsToCache))); });
-self.addEventListener('activate', e => e.waitUntil(caches.keys().then(keys => Promise.all(keys.map(k => { if(k !== CACHE_NAME) return caches.delete(k); })))));
+self.addEventListener('activate', e => e.waitUntil(caches.keys().then(keys => Promise.all(keys.map(k => { if(k !== CACHE_NAME) return caches.delete(k); }))).then(() => self.clients.claim())));
 self.addEventListener('fetch', e => { e.respondWith(fetch(e.request).catch(() => caches.match(e.request))); });
