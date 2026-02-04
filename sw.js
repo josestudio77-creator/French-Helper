7fabed5a006e1bf9692f961a109115472e90513b
josestudio77-creator <josestudio77@gmail.com>
2026-02-04 11:18:12 -0500

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,5 +1,5 @@
-// sw.js - Service Worker V31
-const CACHE_NAME = 'french-helper-v31';
+// sw.js - Service Worker V32
+const CACHE_NAME = 'french-helper-v32';
 const urlsToCache = ['./', './index.html', './icon.png', './manifest.json'];
 self.addEventListener('install', e => { self.skipWaiting(); e.waitUntil(caches.open(CACHE_NAME).then(c => c.addAll(urlsToCache))); });
 self.addEventListener('activate', e => e.waitUntil(caches.keys().then(keys => Promise.all(keys.map(k => { if(k !== CACHE_NAME) return caches.delete(k); }))).then(() => self.clients.claim())));
